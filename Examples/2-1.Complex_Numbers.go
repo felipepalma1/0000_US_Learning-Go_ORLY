80a20a1ef1542a9d69ea00a0cc35e3d5323839a7
Felipe Palma <felipepalmacastro@gmail.com>
2023-08-19 23:01:51 -0400

    Example 2-1. Complex Numbers.

---
+++ Examples/2-1.Complex_Numbers.go
@@ -0,0 +1,19 @@
+package Examples
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+func Example_02_01_Complex_Numbers() {
+	fmt.Println("Complex Numbers")
+	x := complex(2.5, 3.1)
+	y := complex(10.2, 2)
+	fmt.Println(x + y)
+	fmt.Println(x - y)
+	fmt.Println(x * y)
+	fmt.Println(x / y)
+	fmt.Println(real(x))
+	fmt.Println(imag(x))
+	fmt.Println(cmplx.Abs(x))
+}
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/felipepalma1/0000_US_Learning-Go_ORLY/Examples"
+)
 
 func main() {
 	fmt.Println("Learning Go")
+	Examples.Example_02_01_Complex_Numbers()
 }
